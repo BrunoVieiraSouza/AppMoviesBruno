4b7a9d6618ac693bd862ab1c420a981ad9cc7039
Bruno Vieira Souza <bvsouza@yahoo.com.br>
2022-01-14 12:52:50 -0400

    criando classes e xibs das celulas

---
+++ AppMovies/Module/Home/View/MovieCell/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  AppMovies
+//
+//  Created by Bruno Vieira Souza on 14/01/22.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
